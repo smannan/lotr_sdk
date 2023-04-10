9a4234b0457842d2a02372e3ae3b3c160be51e7d
Sonia Mannan <smannan@apple.com>
2023-04-09 21:56:10 -0700

    update dir structure

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='smannan_lotr_sdk',
-    version='3.0',
+    version='4.0',
     author="Sonia Mannan",
     author_email='smannan95@hotmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/smannan/lotr_sdk',
-    keywords='sdk, python',
+    keywords=['sdk', 'python'],
     install_requires=[]
 )
