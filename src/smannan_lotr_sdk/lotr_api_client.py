8471a4c4079ce40ab77e4d44b9accca074dec51d
Sonia Mannan <smannan@apple.com>
2023-04-09 22:25:38 -0700

    modularize files

--- src/smannan_lotr_sdk/lotr_api_client.py
+++ src/smannan_lotr_sdk/lotr_api_client.py
@@ -1,62 +1,6 @@
-import json
-import logging
-import os
-import requests
-import sys
-
-BASE_URL = 'https://the-one-api.dev/v2'
-API_ACCESS_KEY_NAME = 'LOTR_API_ACCESS_KEY'
-
-class Quote:
-	def __init__(self, document):
-		self.quote_id = document['_id']
-		self.movie_id = document['movie']
-		self.character_id = document['character']
-		self.quote = document['dialog']
-
-	def __str__(self):
-		return self.quote
-
-	def __repr__(self):
-		return self.quote
-
-class Movie:
-	def __init__(self, document):
-		self.movie_id = document['_id']
-		self.movie_name = document['name']
-		self.runtime_in_minutes = document['runtimeInMinutes']
-		self.budget_in_millions = document['budgetInMillions']
-		self.box_office_revenue_in_millions = document['boxOfficeRevenueInMillions']
-		self.academy_award_nominations = document['academyAwardNominations']
-		self.academy_award_wins = document['academyAwardWins']
-		self.rotten_tomatoes_score = document['rottenTomatoesScore']
-
-	def __str__(self):
-		return self.movie_name
-
-	def __repr__(self):
-		return self.movie_name
-
-"""
-@param path: the relative path to the resource
-@param params: dictionary with optional query parameters
-
-@return (status, response_json): http status code and response json from get request
-"""
-def get_request(path: str, params={}) -> tuple:
-	# get API access key from environment if available
-	access_key = ''
-	if API_ACCESS_KEY_NAME in os.environ:
-		access_key = os.environ[API_ACCESS_KEY_NAME]
-	else:
-		logging.warning("Access key not found. Set environment variable: export LOTR_API_ACCESS_KEY=<your-api-key>")
-
-	# make get request
-	headers = {"Authorization": "Bearer {0}".format(access_key)}
-	response = requests.get("{0}/{1}".format(BASE_URL, path), headers=headers, params=params)
-
-	# return status code and json
-	return json.loads(response.text), response.status_code
+from http_service import get_request
+from movie import Movie
+from quote import Quote
 
 """
 Get all movies or optionally one movie specified by movie_id
